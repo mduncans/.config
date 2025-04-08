7f4e0af801ec01a603e78025e7cc7808eabbaa8b
Matt Smith <msmith21794@gmail.com>
2025-04-08 08:51:00 -0400

    added lemminx lsp for xml files

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -71,6 +71,11 @@ return {
 				capabilities = capabilities,
 			})
 
+			lspconfig.lemminx.setup({
+				filetypes = { "xml" },
+				capabilities = capabilities
+			})
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>bd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
